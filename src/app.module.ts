28187d952755b1f2e9b5bddbceea6029d2ec7f8b
DucHuy2801 <theflash28012002@gmail.com>
2023-12-13 21:38:09 +0700

    add customer + user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserEntity } from './entity/user.entity';
+import { CustomerEntity } from './entity/customer.entity';
 
 @Module({
     imports: [
@@ -14,9 +17,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            entities: [],
+            entities: [
+                UserEntity,
+                CustomerEntity
+            ],
             synchronize: true,
-        })
+        }),
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
