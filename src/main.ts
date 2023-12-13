174240cb87da2ba2837d357e9624d12410b4e8e3
DucHuy2801 <theflash28012002@gmail.com>
2023-12-13 13:35:05 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /*Swagger config */
+  const config = new DocumentBuilder()
+    .setTitle("BKTravel API")
+    .setDescription('The BKTravel Source API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app
+    .listen(process.env.PORT)
+    .then(() => {
+      console.log(
+        `Application is running on: Swagger UI: http://localhost:${process.env.PORT}/api`
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 bootstrap();
