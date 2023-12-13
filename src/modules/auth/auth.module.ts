ffdc5d0df417e56f116858da57f8de805a58a0bc
DucHuy2801 <theflash28012002@gmail.com>
2023-12-13 21:39:18 +0700

    add import

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { UserEntity } from 'src/entity/user.entity';
 
 @Module({
+  imports: [
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([UserEntity])
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
